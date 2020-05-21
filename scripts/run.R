88cbca8e9e1413ea9d15a777243032860ae890ef
richelbilderbeek <richel@richelbilderbeek.nl>
2020-05-21 19:13:14 +0200

    Do something interesting

--- scripts/run.R
+++ scripts/run.R
@@ -1,10 +1,12 @@
 library(pureseqtmr)
-library(testthat)
+library(knitr)
 
 fasta_filename <- "my_proteome.fasta"
 expect_true(file.exists(fasta_filename))
 
 topology <- predict_topology(fasta_filename)
 
-tally_topology(topology)
+kable(topology)
+
+kable(tally_topology(topology))
 
